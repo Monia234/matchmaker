e70ddbcdad806b157985e878b593b4652bbb1a68
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-28 10:50:41 -0400

    parameterize over the project directory and correct and error

--- plot.py
+++ plot.py
@@ -11,17 +11,19 @@ import match
 
 import jerrington_tools as j
 
-PROJECT_DIR = "/lb/project/gravel"
-IBD_DIR = path.join(PROJECT_DIR,
-        "baharian_projects/HRS/data/dbGaP/AfrAm/phased/3_GERMLINE")
+def main(project_dir):
+    global PROJECT_DIR
+    PROJECT_DIR = project_dir
 
-BED_DIR = path.join(PROJECT_DIR,
-        "barakatt_projects/HRS/results/HRS_AFRAM_20140609/outbed")
+    IBD_DIR = path.join(PROJECT_DIR,
+            "baharian_projects/HRS/data/dbGaP/AfrAm/phased/3_GERMLINE")
 
-make_ibd_path = lambda i: path.join(IBD_DIR,
-        "".join(["AfrAm.chr", str(i), ".IBD.match.gz"]))
+    BED_DIR = path.join(PROJECT_DIR,
+            "barakatt_projects/HRS/results/HRS_AFRAM_20140609/outbed")
 
-def main(args):
+    make_ibd_path = lambda i: path.join(IBD_DIR,
+            "".join(["AfrAm.chr", str(i), ".IBD.match.gz"]))
+
     ibd_chromosomes = map(
             j.compose(ibd.IBDEntry.from_GERMLINE, make_ibd_path),
             xrange(1, 2)) # just chromosome 1 for testing
@@ -29,7 +31,7 @@ def main(args):
     flipcurry2 = j.compose(j.curry2, j.flip)
 
     match_from_ibd_segment__ = match.IBDAncestryMatch.from_ibd_segment
-    my_from_ibd_segment = j.supply(match_from_ibd_segment, {"generate":True})
+    my_from_ibd_segment = j.supply(match_from_ibd_segment__, {"generate":True})
 
     match_from_ibd_segment = flipcurry2(my_from_ibd_segment)(BED_DIR)
 
@@ -39,4 +41,6 @@ def main(args):
     matches = map(j.map_c(match_from_ibd_segment), ibd_chromosomes)
 
 if __name__ == "__main__":
-    main(args)
+    if len(args) != 2:
+        raise ValueError("incorrect number of arguments")
+    main(args[1])
