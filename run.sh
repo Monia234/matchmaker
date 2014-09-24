7a3defd075bf1ad92cc606cd032bf98a3817e138
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-09-24 16:44:40 -0400

    prepend classifier string to output of this testing branch

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME="$(date +%F-%T)"
+NAME="sharedanctest-$(date +%F-%T)"
 
 python plot.py project "plots/$NAME" > "logs/${NAME}.log"
 notify-send "IBD and local ancestry plots" "Finished generating 'plots/$NAME'."
