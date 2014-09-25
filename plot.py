ffdba08841fea3c2915c2785918b5cdc35275cca
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-09-25 10:11:26 -0400

    parameterize displaying of debug shared ancestry rects

--- plot.py
+++ plot.py
@@ -23,7 +23,7 @@ import operator as op
 
 from PIL import Image, ImageDraw
 
-def plot_matches(matches):
+def plot_matches(matches, debug_shared_ancestry=False):
     """ Create an image configured as in ibd_anc_plt_config.py displaying the
         given list of matches. The list of matches must be ordered, but this is
         not checked!
@@ -207,26 +207,27 @@ def plot_matches(matches):
                                 upper_bound, ") "
                                 "-> DRAW [", ", ".join(map(str, rect3)), "] ",
                                 seg.code.name, sep='')
-        for shared_segment in entry.shared_segments:
-            rect_height = ENTRY_HEIGHT
+        if debug_shared_ancestry:
+            for shared_segment in entry.shared_segments:
+                rect_height = ENTRY_HEIGHT
 
-            segment_width_true = len(shared_segment.interval_bp)
-            segment_width = scale_x(segment_width_true)
+                segment_width_true = len(shared_segment.interval_bp)
+                segment_width = scale_x(segment_width_true)
 
-            xstart = jt.intround(
-                    scale_x(shared_segment.interval_bp.start)
-                    + ibd_center_offset)
-            xend = jt.intround(xstart + segment_width)
+                xstart = jt.intround(
+                        scale_x(shared_segment.interval_bp.start)
+                        + ibd_center_offset)
+                xend = jt.intround(xstart + segment_width)
 
-            shared_rect = Image.new("RGBA",
-                    (jt.intround(segment_width),
-                        jt.intround(rect_height)),
-                    color=conf.SHARED_ANC_DEBUG_COLOR)
+                shared_rect = Image.new("RGBA",
+                        (jt.intround(segment_width),
+                            jt.intround(rect_height)),
+                        color=conf.SHARED_ANC_DEBUG_COLOR)
 
-            sa_y0 = jt.intround(
-                    i * (ENTRY_HEIGHT + conf.INTER_ENTRY_MARGIN))
+                sa_y0 = jt.intround(
+                        i * (ENTRY_HEIGHT + conf.INTER_ENTRY_MARGIN))
 
-            im.paste(shared_rect, (xstart, sa_y0), mask=shared_rect)
+                im.paste(shared_rect, (xstart, sa_y0), mask=shared_rect)
     return im
 
 def n_most(seq, n, comp=op.lt):
