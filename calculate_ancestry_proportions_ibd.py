4524aa4c4725160569a312df393700e8c78531fd
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-09-30 16:04:46 -0400

    add command-line argument parser

--- calculate_ancestry_proportions_ibd.py
+++ calculate_ancestry_proportions_ibd.py
@@ -2,17 +2,42 @@
 
 from __future__ import print_function
 
+import sys
+from sys import argv as args
+
+from itertools import imap
+
 import jerrington_tools as jt
 import match
 import bed
 import ibd
 import dataset_utils
 
-def main(ibd_paths, outbed_path, ibd_filterf):
+def show_usage():
+    errprint = jt.supply(print, {"file":sys.stderr})
+    map(errprint, [
+        "calculate_ancestry_proportions_ibd.py -- calculate the proportion of",
+        "  each ancestry in a given population within IBD regions.",
+        "",
+        "usage: ./calculate_ancestry_proportions_ibd.py",
+        "  (-d|--dataset) <dataset filter> (-b|--beddir) <rfmix outbed directory>",
+        "  ((-i|--ibd) <germline output file for one chromosome>)+",
+        ""])
+
+    map(errprint, ["Arbitrarily many IBD chromosome files can be given, in which ",
+        "case statistics are calculated for each chromosome and then averaged."])
+
+def main(ibd_paths, outbed_path, ibd_filter):
+    if ibd_filter is dataset_utils.is_afram_hrs:
+        INDIVS = set(jt.with_file(jt.map_c(lambda x: x[:-1]), indiv_list_path))
+        ibd_filterf = jt.curry2(ibd_filter)(INDIVS)
+    else:
+        ibd_filterf = ibd_filter
+
     matches = match.IBDAncestryMatch.from_ibds_and_bedpath(
             ibd_paths, outbed_path, ibd_filterf)
 
-    ancestry_sizes = map(
+    ancestry_sizes = imap(
             lambda m: m.calculate_ibd_ancestry_sizes(),
             matches)
 
@@ -32,6 +57,52 @@ def main(ibd_paths, outbed_path, ibd_filterf):
         print(code, size / total_ibd_length, sep='\t')
 
 if __name__ == "__main__":
-    main(["project/baharian_projects/MergedData/phased/3_GERMLINE/cMcorrected/MERGED_chr1.cM.IBD.match.gz"],
-        "project/barakatt_projects/HRS/results/SCCS_notphased_20140923/outbed",
-        dataset_utils.is_sccs)
+    if len(args) == 1:
+        print("No command-line arguments given.", file=sys.stderr)
+        show_usage()
+        sys.exit(1)
+
+    if jt.any_eq(args[1], ["-h", "--help"]):
+        show_usage()
+        sys.exit(0)
+
+    ibds = []
+    beddir = None
+    dataset = "id"
+
+    i = 1
+    while i < len(args):
+        arg = args[i]
+        nextarg = lambda: args[i + 1]
+        if jt.any_eq(arg, ["-i", "--ibd"]):
+            ibds.append(nextarg())
+            i += 1
+        elif jt.any_eq(arg, ["-b", "--bed-dir"]):
+            beddir = nextarg()
+            i += 1
+        elif jt.any_eq(arg, ["-d", "--dataset"]):
+            dataset = nextarg()
+            i += 1
+        else:
+            print("Unrecognized command-line argument:", arg, file=sys.stderr)
+            show_usage()
+            sys.exit(1)
+        i += 1
+
+    if not (ibds and beddir and dataset):
+        print("One or more missing command-line arguments.", file=sys.stderr)
+        show_usage()
+        sys.exit(1)
+
+    filters = {"sccs":dataset_utils.is_sccs, "hrs":dataset_utils.is_afram_hrs,
+            "id":jt.const(True)}
+
+    if not jt.any_eq(dataset, ["sccs", "hrs", "id"]):
+        print("Unknown dataset filter. Known dataset filters are:\n" +
+                ", ".join(filters.keys()) +
+                "\nThe 'id' filter is a filter that simply"
+                "does not filter elements at all.", file=sys.stderr)
+        show_usage()
+        sys.exit(1)
+
+    main(ibds, beddir, dataset_utils.is_sccs)
